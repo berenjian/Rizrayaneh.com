619c2aec71bacc227ebb40e88f7d101a531f91cb
Amirmahdi Berenjian <a.brenjian@1dws.com>
2012-09-11 14:08:31 +0430

    add loading

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+window.onload = function(){
+            document.body.className = '';
+            setTimeout(function(){
+                document.getElementById('loading').style.display='none';
+            },2000)
+}
